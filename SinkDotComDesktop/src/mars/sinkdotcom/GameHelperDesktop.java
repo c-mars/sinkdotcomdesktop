8cf08f1f6307d878790273db99abdc63a9988616
mars <mars@mars-lifebook>
2014-02-22 01:47:44 +0200

    Single-line game for Chapter 5. With bug of hits count.

--- SinkDotComDesktop/src/mars/sinkdotcom/GameHelperDesktop.java
+++ SinkDotComDesktop/src/mars/sinkdotcom/GameHelperDesktop.java
@@ -1,5 +1,24 @@
 package mars.sinkdotcom;
 
-public class GameHelperDesktop {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class GameHelperDesktop extends GameHelper {
+
+	public String getUserInput(String prompt) {
+		String inputLine = null;
+		System.out.print(prompt + " ");
+		try {
+			BufferedReader is = new BufferedReader(
+					new InputStreamReader(System.in));
+			inputLine = is.readLine();
+			if (inputLine.length() == 0 )
+				return null;
+		} catch (IOException e) {
+			System.out.println("IOException: " + e);
+		}
+		return inputLine;
+	}
+	
 }
